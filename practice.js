b7628648ccf08c922ae10a449dd97d636706dcfc
me-agrim-rehan <agrimrehan.work@gmail.com>
2026-04-17 23:06:12 +0530

    day 2 - task 1

--- practice.js
+++ practice.js
@@ -1,30 +1,42 @@
-function toss (){   
-   const Prob = Math.random() 
-   if (Prob > 0.5){
-        return "Team A";
-   }
-   else {
-    return "Team B"
-   };
-}          // task one 
+// function toss (){   
+//    const Prob = Math.random() 
+//    if (Prob > 0.5){
+//         return "Team A";
+//    }
+//    else {
+//     return "Team B"
+//    };
+// }          // task one 
 
 
 
-function decision (){
-   const desc = Math.random() * 10;
-    if(desc > 5){
-        return "Bat"
-    }else{
-        return "Bowl"
-    };
-};     // task two
+// function decision (){
+//    const desc = Math.random() * 10;
+//     if(desc > 5){
+//         return "Bat"
+//     }else{
+//         return "Bowl"
+//     };
+// };     // task two
 
 
-function tossAndDecision (){
-    const tossResult = toss();
-    const decisionResult = decision();
+// function tossAndDecision (){
+//     const tossResult = toss();
+//     const decisionResult = decision();
     
-    return tossResult + " won the toss and has elected to " + decisionResult +" first!";
-};  // task 3
+//     return tossResult + " won the toss and has elected to " + decisionResult +" first!";
+// };  // task 3
 
-console.log(tossAndDecision()); //final result    
+// console.log(tossAndDecision()); //final result    
+
+
+const TeamA = ["player1", "player2", "player3", "player4", "player5" ];
+const TeamB = ["player_a", "player_b", "player_c", "player_d", "player_e"];
+
+console.log(TeamA);
+console.log(TeamB); // day 2 task 1
+
+
+
+
+     
