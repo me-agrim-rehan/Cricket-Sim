c9844553b50bfdbeeae8f0fa3e98854ad9c95ea7
me-agrim-rehan <agrimrehan.work@gmail.com>
2026-04-18 00:01:41 +0530

    Day 2 prac -- combined the toss system to random player selection system

--- practice.js
+++ practice.js
@@ -1,55 +1,58 @@
-// function toss (){   
-//    const Prob = Math.random() 
-//    if (Prob > 0.5){
-//         return "Team A";
-//    }
-//    else {
-//     return "Team B"
-//    };
-// }          // task one 
+function comments() {
+    // console.log(TeamA);
+    // console.log(TeamB); // day 2 task 1
 
+    // for ( let i = 0 ; i < TeamA.length ; i++){
+    //     console.log(TeamA[i]);
+    // }  //day 2 task 2
 
-
-// function decision (){
-//    const desc = Math.random() * 10;
-//     if(desc > 5){
-//         return "Bat"
-//     }else{
-//         return "Bowl"
-//     };
-// };     // task two
-
-
-// function tossAndDecision (){
-//     const tossResult = toss();
-//     const decisionResult = decision();
-    
-//     return tossResult + " won the toss and has elected to " + decisionResult +" first!";
-// };  // task 3
-
-// console.log(tossAndDecision()); //final result    
-
-
-const TeamA = ["player1", "player2", "player3", "player4", "player5" ];
-const TeamB = ["player_a", "player_b", "player_c", "player_d", "player_e"];
-
-// console.log(TeamA);
-// console.log(TeamB); // day 2 task 1
-
-// for ( let i = 0 ; i < TeamA.length ; i++){
-//     console.log(TeamA[i]);
-// }  //day 2 task 2
-
-function randomPlayer(team){
-    return team[Math.floor(Math.random() * team.length)];   
 }
 
-console.log(randomPlayer(TeamA)); 
+
+
+const TeamA = ["player1", "player2", "player3", "player4", "player5"];
+const TeamB = ["player_a", "player_b", "player_c", "player_d", "player_e"];
+
+
+function toss() {
+    const Prob = Math.random()
+    if (Prob > 0.5) {
+        return "TeamA";
+    }
+    else {
+        return "TeamB"
+    };
+}          // task one 
 
 
 
+function randomPlayer(team) {
+    return team[Math.floor(Math.random() * team.length)];
+}
 
+function chooseTeam(tossResult) {
+    if (tossResult === "TeamA") {
+        return TeamA;
+    }else {
+        return TeamB;
+    }
+}
 
+function decision() {
+    const desc = Math.random() * 10;
+    if (desc > 5) {
+        return "Bat"
+    } else {
+         return "Bowl"
+    };
+};     // task two
 
+function tossAndDecision() {
+    const tossResult = toss();
+    const randomPlayerResult = randomPlayer(chooseTeam(tossResult));
 
-     
+    return tossResult + " has won the toss!" + "\n" + "Random player : " + randomPlayerResult + " " + "has chosen to " + decision() + " first!";
+};  // task 3
+
+console.log(tossAndDecision()); //final result    
+
